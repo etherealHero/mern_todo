1096b8c59092f3f0bd01e46ec8db772a77d94ceb
etherealhero <gutcan4312@gmail.com>
2023-05-07 07:54:19 +0000

    add PWA

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,47 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import { VitePWA } from "vite-plugin-pwa"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      outDir: "dist",
+      manifest: {
+        name: "MERN Tasks",
+        short_name: "MTasks",
+        description:
+          "organize your tasks with MTasks PWA application. Based on MongoDB-Express-React-NodeJS stack",
+        theme_color: "#131D34",
+        icons: [
+          {
+            src: "/icons/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "/icons/icon-256x256.png",
+            sizes: "256x256",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "/icons/icon-384x384.png",
+            sizes: "384x384",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "/icons/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+        ],
+      },
+    }),
+  ],
 })
